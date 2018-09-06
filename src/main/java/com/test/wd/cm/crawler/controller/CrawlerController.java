d7b27b080811ce224fc2cb846eabbfdafca26e65
ciaranmolloy <ciarangmolloy@hotmail.com>
2018-09-06 22:30:04 +0100

    Tidied up service and controller and updated Readme

--- src/main/java/com/test/wd/cm/crawler/controller/CrawlerController.java
+++ src/main/java/com/test/wd/cm/crawler/controller/CrawlerController.java
@@ -1,30 +1,26 @@
 package com.test.wd.cm.crawler.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.test.wd.cm.crawler.service.CrawlerService;
-import com.test.wd.cm.crawler.service.impl.CrawlerServiceImpl;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
 @RestController
 @RequestMapping("/test")
 public class CrawlerController {
 	
-	// TODO - Create Bean and Autowire
-	private final CrawlerServiceImpl crawlerServiceImpl = new CrawlerServiceImpl();
-	private CrawlerService crawlerService;
+	@Autowired
+	private CrawlerService service;
 
 	@RequestMapping(value = "/crawl", method = GET)
-	public String crawlWipro() {
-		crawlerService = crawlerServiceImpl.getService();
+	public List<String> crawlWipro() {
 		
-		final String html = crawlerService.getHTML();
-		
-		return html;
+		return service.getHrefLinks();
 	}
 
 }
--- src/main/java/com/test/wd/cm/crawler/service/CrawlerService.java
+++ src/main/java/com/test/wd/cm/crawler/service/CrawlerService.java
@@ -1,14 +1,57 @@
 package com.test.wd.cm.crawler.service;
 
-import feign.RequestLine;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-/**
- * @author Ciaran
- *
- */
-public interface CrawlerService {
-	
-	@RequestLine("GET /")
-	public String getHTML();
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+@Component
+public class CrawlerService {
+	private static final String HREF = "href";
+	private static final String ANCHOR_HREF = "a[href]";
+
+	@Value("${wipro.url}")
+	private String url;
+
+	public List<String> getHrefLinks() {
+
+		Document doc;
+		Elements links;
+		StringBuilder string = null;
+		List strings = Collections.EMPTY_LIST;
+
+		try {
+			doc = Jsoup.connect(url).get();
+			links = doc.select(ANCHOR_HREF);
+			string = new StringBuilder();
+
+			strings = createLinksList(links, string);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return strings;
+	}
+
+	private List<String> createLinksList(final Elements links, final StringBuilder string) {
+		final List<String> strings = new ArrayList();
+		for (final Element link : links) {
+			final String linkUrl = link.attr(HREF);
+
+			if (linkUrl.contains(url)) {
+				string.append("-- ").append(linkUrl).append("\n");
+				strings.add(linkUrl);
+			}
+		}
+		return strings;
+	}
 }
