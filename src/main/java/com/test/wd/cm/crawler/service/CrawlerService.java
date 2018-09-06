1bc8260b864856fbd2acdf1dfb68855d0a8b9073
ciaranmolloy <ciarangmolloy@hotmail.com>
2018-09-06 18:14:56 +0100

    Added project files

--- src/main/java/com/test/wd/cm/crawler/service/CrawlerService.java
+++ src/main/java/com/test/wd/cm/crawler/service/CrawlerService.java
@@ -3,10 +3,15 @@
  */
 package com.test.wd.cm.crawler.service;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @author Ciaran
  *
  */
+@Service
 public class CrawlerService {
+	
+	
 
 }
